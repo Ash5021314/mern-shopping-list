518bd2b9cc1cf24565c54a04536cee1b4fe6d4cd
unknown <ashkapoghosyan@gmail.com>
2019-12-11 10:53:16 +0300

    commit

--- server.js
+++ server.js
@@ -14,35 +14,27 @@ app.use(bodyParser.json())
 
 //DB Config
 
-// const db = require('./config/keys').mongoURI
-const db = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/test'
+const db = require('./config/keys').mongoURI
+// const db = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/test'
 
 
 //connect to Mongo
-mongoose.connect(db, {useNewUrlParser: true}, function (error, db) {
-  console.log('errorMessage', error)
-  db.listCollections().toArray(function (err, collection) {
-    console.log('errorMessage', error)
-    console.log('collections', collection)
-    db.close()
-
+mongoose.connect(db, {useNewUrlParser: true})
+  .then(() => {
+    console.log('mongodb connected...')
   })
-})
-//   .then(() => {
-//     console.log('mongodb connected...')
-//   })
-//   .catch((err => console.log(err)))
-//
-// app.use('/api/items', items)
-// // Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//   // set static folder
-//   app.use(express.static('client/build'))
-//
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   })
-//
-// }
-// const port = process.env.PORT || 5000
-// app.listen(port, () => console.log(`Server started on port ${port}`))
+  .catch((err => console.log(err)))
+
+app.use('/api/items', items)
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server started on port ${port}`))
